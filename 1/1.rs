3eb539e493c4163974064c8f26b23c8394f604cb
Adam Šír <5871877+adamsir@users.noreply.github.com>
2021-01-12 15:27:19 +0100

    Update 1.rs

--- 1/1.rs
+++ 1/1.rs
@@ -4,9 +4,9 @@ use std::io::prelude::*;
 fn main() -> std::io::Result<()> {
     let mut file = File::open("input.txt")?;
     let mut contents = String::new();
-    let kokot = file.read_to_string(&mut contents)?;
+    let contents = file.read_to_string(&mut contents)?;
      
-    println!("{}", kokot);
+    println!("{}", contents);
 
     Ok(())
 }
